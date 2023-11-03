1beb593d2219b797fa873c3ce151f9866ad7822a
Sven Fiergolla <sven.fiergolla@gmail.com>
2023-11-03 10:01:57 +0100

    Algo: minor improvements and added some java doc to the important parts

--- src/main/kotlin/io/github/fierg/algo/Decomposer.kt
+++ src/main/kotlin/io/github/fierg/algo/Decomposer.kt
@@ -12,8 +12,21 @@ import io.github.fierg.model.result.Factor
 import io.github.fierg.model.result.Cover
 import kotlinx.coroutines.*
 
+/**
+ * The `Decomposer` class represents a decomposer that processes boolean arrays to find covers based on various composition modes and settings.
+ *
+ * @param state             The default state value (default is `true`).
+ * @param mode              The composition mode used for decomposition (default is `CompositionMode.SHORTEST_PERIODS`).
+ * @param deltaWindowAlgo   The delta window algorithm used (default is `0`).
+ * @param threshold         The coverage threshold for finding covers (default is `1.0`).
+ */
 class Decomposer(state: Boolean = true, private val mode: CompositionMode = CompositionMode.SHORTEST_PERIODS, private val deltaWindowAlgo: Int = 0, private val threshold: Double = 1.0) {
 
+    /**
+     * Constructs a `Decomposer` object based on the provided `Options` object.
+     *
+     * @param options The `Options` object containing decomposition parameters.
+     */
     constructor(options: Options) : this(options.state, options.compositionMode, options.deltaWindowAlgo, options.threshold)
 
     private val applyDeltaWindow = deltaWindowAlgo > 0
@@ -21,6 +34,12 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
     private var singleDebugLog = true
     private var nrDigits = 3
 
+    /**
+     * Finds composite covers for a given graph and returns a list of `Cover` objects.
+     *
+     * @param graph The graph to find covers for.
+     * @return A list of `Cover` objects representing composite covers.
+     */
     fun findComposite(graph: EPTGraph): List<Cover> {
         val covers = mutableListOf<Cover>()
 
@@ -36,6 +55,12 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return covers
     }
 
+    /**
+     * Finds a cover for the given boolean array based on the specified composition mode and settings.
+     *
+     * @param input The boolean array to find a cover for.
+     * @return A `Cover` object representing the composite cover.
+     */
     fun findCover(input: BooleanArray): Cover {
         nrDigits = input.size.digits()
         val periods = getFactorSequence(input.size).toList()
@@ -51,6 +76,13 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return getCover(input, factorIndex, factors, periods)
     }
 
+    /**
+     * Retrieves a list of outlier indices in the input array based on the provided cover.
+     *
+     * @param input The input boolean array.
+     * @param cover The cover to identify outliers.
+     * @return A list of outlier indices.
+     */
     fun getOutliers(input: BooleanArray, cover: BooleanArray): List<Int> {
         val expandedCover = BooleanArray(input.size) { !stateToReplace }
         expandedCover.applyPeriod(cover, stateToReplace)
@@ -58,6 +90,11 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return expandedCover.indices.filter { input[it] == stateToReplace && expandedCover[it] != stateToReplace }
     }
 
+    /**
+     * Analyzes a `Cover` object and logs information about the decomposition.
+     *
+     * @param result The `Cover` object to analyze.
+     */
     fun analyzeCover(result: Cover) {
         Logger.info(
             "Found decomposition with ${String.format("%${nrDigits}d", (result.size.toDouble() / result.target.size * 100).toInt())}% original size (${
@@ -76,6 +113,15 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         }
     }
 
+    /**
+     * Retrieves a composite cover for the input array based on the composition mode and settings.
+     *
+     * @param input         The input boolean array.
+     * @param factorIndex   A map of factor sizes and their corresponding indices.
+     * @param factors       An array of factors to consider.
+     * @param periods       A list of period sizes to use for decomposition.
+     * @return A `Cover` object representing the composite cover.
+     */
     private fun getCover(input: BooleanArray, factorIndex: Map<Int, Int>, factors: Array<Factor>, periods: List<Int>): Cover {
         val cover = Cover(input, stateToReplace, if (this.mode == CompositionMode.CLEAN_QUOTIENTS) Operator.AND else Operator.OR)
         when (mode) {
@@ -122,6 +168,14 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return cover
     }
 
+    /**
+     * Retrieves a list of factors for the input array based on the provided factor sizes and indices.
+     *
+     * @param input         The input boolean array.
+     * @param factorIndex   A map of factor sizes and their corresponding indices.
+     * @param periods       A list of period sizes to use for decomposition.
+     * @return An array of `Factor` objects representing the factors.
+     */
     private fun getFactors(input: BooleanArray, factorIndex: Map<Int, Int>, periods: List<Int>): Array<Factor> {
         val factors = Array(factorIndex.size) { Factor(BooleanArray(0), emptyList()) }
         val jobs = mutableListOf<Deferred<Unit>>()
@@ -136,6 +190,14 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return factors
     }
 
+    /**
+     * Computes clean quotients asynchronously.
+     *
+     * @param input         The input boolean array.
+     * @param factorIndex   A map of factor sizes and their corresponding indices.
+     * @param factors       An array of factors to consider.
+     * * @return An array of `Factor` objects representing the factors.
+     */
     @OptIn(DelicateCoroutinesApi::class)
     private fun computeCleanQuotients(input: BooleanArray, factorSize: Int, factors: Array<Factor>, factorIndex: Int) = GlobalScope.async {
         val coverArray = BooleanArray(factorSize) { !stateToReplace }
@@ -147,6 +209,14 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return@async
     }
 
+    /**
+     * Computes factors asynchronously.
+     *
+     * @param input         The input boolean array.
+     * @param factorIndex   A map of factor sizes and their corresponding indices.
+     * @param periods       A list of period sizes to use for decomposition.
+     * @return An array of `Factor` objects representing the factors.
+     */
     @OptIn(DelicateCoroutinesApi::class)
     private fun computeFactors(input: BooleanArray, factorSize: Int, factors: Array<Factor>, factorIndex: Int) = GlobalScope.async {
         val coverArray = BooleanArray(factorSize) { !stateToReplace }
@@ -159,6 +229,14 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return@async
     }
 
+    /**
+     * Checks if a given subsequence in the input array is periodic based on the specified factor size.
+     *
+     * @param array     The input boolean array.
+     * @param index     The starting index of the subsequence to check.
+     * @param factor    The factor size for the periodicity check.
+     * @return `true` if the subsequence is periodic; otherwise, `false`.
+     */
     private fun isPeriodic(array: BooleanArray, index: Int, factor: Int): Boolean {
         var position = (index + factor) % array.size
         while (position != index) {
@@ -171,6 +249,12 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         return true
     }
 
+    /**
+     * Retrieves a sequence of factor sizes based on the composition mode.
+     *
+     * @param input The size of the input array.
+     * @return A sequence of factor sizes.
+     */
     private fun getFactorSequence(input: Int): Sequence<Int> {
         return when (mode) {
             CompositionMode.SHORTEST_PERIODS, CompositionMode.FOURIER_TRANSFORM, CompositionMode.CLEAN_QUOTIENTS -> input.factors()
@@ -178,6 +262,9 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         }
     }
 
+    /**
+     * Logs information about the decomposition and the composition mode.
+     */
     private fun logInfo() {
         Logger.info("Searching for composite... (Looking for $stateToReplace values while decomposing)")
         when (mode) {
@@ -188,6 +275,12 @@ class Decomposer(state: Boolean = true, private val mode: CompositionMode = Comp
         }
     }
 
+    /**
+     * Analyzes multiple graphs and finds composite covers for each graph up to a specified index.
+     *
+     * @param upTo The index of the graph to analyze.
+     * @return A list of lists of `Cover` objects representing composite covers for multiple graphs.
+     */
     fun analyzeAllGraphs(upTo: Int): List<List<Cover>> {
         val result = mutableListOf<List<Cover>>()
         for (i in 0..upTo) {
--- src/main/kotlin/io/github/fierg/model/result/Factor.kt
+++ src/main/kotlin/io/github/fierg/model/result/Factor.kt
@@ -1,11 +1,31 @@
 package io.github.fierg.model.result
 
-import io.github.fierg.logger.Logger
-
+/**
+ * The `Factor` class represents a factor with a set of boolean values (cover) and a list of outliers.
+ * It is a subclass of the `CleanQuotient` class, inheriting its properties and methods.
+ *
+ * @param cover     The array of boolean values representing the factor.
+ * @param outliers  The list of indices representing outliers in the factor.
+ */
 class Factor(var cover: BooleanArray, val outliers: List<Int>) : CleanQuotient(cover) {
+    /**
+     * Constructs a `Factor` object using a boolean array to represent the cover and a list of outliers.
+     *
+     * @param cover     The array of boolean values representing the factor.
+     * @param outliers  The list of indices representing outliers in the factor.
+     */
     constructor(cover: Array<Boolean>, outliers: List<Int>) : this(cover = cover.toBooleanArray(), outliers)
 
     companion object {
+
+        /**
+         * Static method to calculate outliers for clean quotients using the provided parameters.
+         *
+         * @param target          The boolean array representing the target state.
+         * @param stateToReplace  The boolean value to replace in the factors.
+         * @param factors         A collection of boolean arrays representing factors.
+         * @return A mutable list of integer indices representing outliers.
+         */
         fun getOutliersForCleanQuotients(target: BooleanArray, stateToReplace: Boolean, factors: Collection<BooleanArray>): MutableList<Int> {
             val outliers = mutableListOf<Int>()
             target.forEachIndexed { index, state ->
@@ -21,11 +41,22 @@ class Factor(var cover: BooleanArray, val outliers: List<Int>) : CleanQuotient(c
         }
     }
 
+    /**
+     * Returns a string representation of the `Factor` object, displaying the cover as a list of 0s and 1s.
+     *
+     * @return A string representation of the `Factor` object.
+     */
     override fun toString(): String {
         return "${cover.map { if (it) "1" else "0" }}"
         //return "${cover.size}:${outliers.size}"
     }
 
+    /**
+     * Retrieves the boolean value at the specified index in the cover array.
+     *
+     * @param index The index of the element to retrieve.
+     * @return The boolean value at the specified index in the cover array.
+     */
     fun get(index: Int): Boolean {
         return cover[index % cover.size]
     }
