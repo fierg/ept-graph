a640a9bf2cef6ddfb4227fcd09a9300b3fe4a0ad
Sven Fiergolla <sven.fiergolla@gmail.com>
2023-11-03 12:49:18 +0100

    Algo: Readded plot generation

--- src/test/kotlin/io/github/fierg/chart/ChartGenerator.kt
+++ src/test/kotlin/io/github/fierg/chart/ChartGenerator.kt
@@ -4,35 +4,35 @@ import io.github.fierg.algo.Decomposer
 import io.github.fierg.analysis.Visualizer
 import io.github.fierg.data.DotEnvParser
 import io.github.fierg.logger.Logger
-import io.github.fierg.model.style.DefaultPlotStyle.Companion.DEFAULT_HEIGHT
-import org.jetbrains.letsPlot.GGBunch
 import org.junit.Ignore
 import org.junit.Test
 
 @Ignore
 class ChartGenerator {
 
-
-
     @Test
     @Ignore //Ignored in default test suit because of long run time of around 2-3 minutes
-    fun testAnalyzerAllGraphs() {
+    fun plotValuesCoveredByFactors() {
         Logger.info("Analyzing all graphs, reading options from .env file")
-
-        val upTo = 61
+        Logger.setLogLevelToQuiet()
+        val upTo = 20
         val options = DotEnvParser.readDotEnv()
         val decomposer = Decomposer(options)
         val evalResult = decomposer.analyzeAllGraphs(upTo)
-        val plot = Visualizer.createCoverByFactorPlot(evalResult.flatten())
+        Logger.setLogLevelToDebug()
 
+        val factorPlot1 = Visualizer.createCoverByFactorPlot(evalResult.flatten())
+        Visualizer.savePlotToFile("all-relative-values-by-factor-size.png", factorPlot1)
 
-        Visualizer.savePlotToFile("all-covered-values-pie-chart-combined.png", plot)
+        val coverPlot1 = Visualizer.createCoverByDecompositionPlot(evalResult.flatten())
+        Visualizer.savePlotToFile("all-relative-values-by-cover-size.png", coverPlot1)
+
+        val factorPlot2 = Visualizer.createCoverByFactorPlot(evalResult.flatten(), useAverage = true)
+        Visualizer.savePlotToFile("all-relative-values-by-factor-size-average.png", factorPlot2)
+
+        val coverPlot2 = Visualizer.createCoverByDecompositionPlot(evalResult.flatten(), useAverage = true)
+        Visualizer.savePlotToFile("all-relative-values-by-cover-size-average.png", coverPlot2)
 
         Logger.info("Done.")
     }
-
-
-
-
-
 }
