ee53e9bc4ab55f1a95e04bf4837d51e3583b1bd4
Sven Fiergolla <sven.fiergolla@gmail.com>
2023-11-13 11:51:46 +0100

    Build: trying without explicit bibtex

--- tex-build.sh
+++ tex-build.sh
@@ -3,7 +3,7 @@
 echo "Building from bash script..."
 cd thesis || echo "Folder not found!"
 
-latexmk -pdf -bibtex -f -silent thesis.tex
+latexmk -pdf -f -silent thesis.tex
 
 printf "\n\n\nDONE\n\n\n"
 exit 0
