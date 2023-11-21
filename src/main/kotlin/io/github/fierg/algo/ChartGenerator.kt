e8332d4e6452191967364ab38f2fb379f721bdcc
Sven Fiergolla <sven.fiergolla@gmail.com>
2023-11-21 11:00:32 +0100

    Algo: improved chart generation structure

--- src/main/kotlin/io/github/fierg/algo/ChartGenerator.kt
+++ src/main/kotlin/io/github/fierg/algo/ChartGenerator.kt
@@ -1,45 +1,13 @@
-package io.github.fierg.chart
+package io.github.fierg.algo
 
-import io.github.fierg.algo.Decomposer
 import io.github.fierg.analysis.Analyzer
 import io.github.fierg.analysis.Visualizer
 import io.github.fierg.data.DotEnvParser
 import io.github.fierg.logger.Logger
 import io.github.fierg.model.options.DecompositionMode
-import org.junit.Ignore
-import org.junit.Test
 
-@Ignore
 class ChartGenerator {
 
-    @Test
-    @Ignore //Ignored in default test suit because of long run time of around 2-3 minutes
-    fun generateAveragedPlots() {
-        Logger.info("Analyzing all graphs in all modes")
-        Logger.info("This might take a while...")
-        DecompositionMode.values()
-            .forEach { compositionMode ->
-                Logger.info("Analyzing all graphs, reading options from .env file, using composition mode ${compositionMode.name}")
-                Logger.setLogLevelToQuiet()
-                val upTo = 2
-                val options = DotEnvParser.readDotEnv()
-                options.decompositionMode = compositionMode
-                val decomposer = Decomposer(options)
-                val evalResult = decomposer.analyzeAllGraphs(upTo)
-                Logger.setLogLevelToDebug()
-
-                val factorPlot1 = Analyzer.createCoverByFactorPlotNormalized(evalResult.flatten())
-                Visualizer.savePlotToFile("${options.decompositionMode.name}-all-relative-values-by-factor-size.png", factorPlot1)
-
-                val coverPlot1 = Analyzer.createCoverByDecompositionPlot(evalResult.flatten())
-                Visualizer.savePlotToFile("${options.decompositionMode.name}-all-relative-values-by-cover-size.png", coverPlot1)
-            }
-        Logger.info("Done.")
-    }
-
-
-    @Test
-    @Ignore //Ignored in default test suit because of long run time of around 2-3 minutes
     fun generatePlots() {
         Logger.info("Analyzing all graphs in all modes")
         Logger.info("This might take a while...")
@@ -54,11 +22,19 @@ class ChartGenerator {
                 val evalResult = decomposer.analyzeAllGraphs(upTo)
                 Logger.setLogLevelToDebug()
 
+                val factorPlot1 = Analyzer.createCoverByFactorPlotNormalized(evalResult.flatten())
+                Visualizer.savePlotToFile("${options.decompositionMode.name}-all-relative-values-by-factor-size.png", factorPlot1)
+
                 val factorPlot = Analyzer.createCoverByFactorPlot(evalResult.flatten())
                 Visualizer.savePlotToFile("${options.decompositionMode.name}-all-values-by-factor-size.png", factorPlot)
+
                 val factorPlot2 = Analyzer.createCoverByFactorPlotNormalizedByCover(evalResult.flatten())
                 Visualizer.savePlotToFile("${options.decompositionMode.name}-all-values-by-factor-size-normalized-by-cover.png", factorPlot2)
             }
         Logger.info("Done.")
     }
+}
+
+fun main() {
+    ChartGenerator().generatePlots()
 }
